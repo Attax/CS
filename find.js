1600dcc8db0bc9c55763fd38cbd32a14e0e8bbcc
attax <westooy@gmail.com>
2017-07-25 11:58:49 +0800

    添加二叉树

--- find.js
+++ find.js
@@ -43,3 +43,68 @@ function findArr2(num, start, end){
 		}
 	}
 }
+
+/**
+ * 二叉树
+ * binaryTreeSearch
+ */
+/*
+节点
+{
+	value:	数
+	l:		左边
+	r:		右边
+}
+*/
+
+//根节点
+var root=null;
+
+function bst_add(node, n){
+    //第一个
+	if(root==null){
+		//alert('创建根节点：'+n);
+		root={value: n, l: null, r: null};
+	}
+	else{
+        //重复
+		if(node.value==n){
+			//alert(n+'重复了，不要');
+			return;
+		}else{
+			//alert('要插入的是：'+n+'，来到了：'+node.value);
+				//右
+			if(node.value<n){
+				alert('试图往右走');
+				if(node.r)		//右边有东西
+				{
+					alert('右边有东西，继续');
+					bst_add(node.r, n);
+				}
+				else			//右边没东西
+				{
+					alert('右边没了，把'+n+'添加到'+node.value+'的右边');
+					node.r={value: n, l: null, r: null};
+				}
+			}
+			else				//左
+			{
+				alert('试图往左走');
+				if(node.l)
+				{
+					alert('左边有东西，继续');
+					bst_add(node.l, n);
+				}
+				else
+				{
+					alert('左边没了，把'+n+'添加到'+node.value+'的左边');
+					node.l={value: n, l: null, r: null};
+				}
+			}
+		}
+	}
+}
+
+for(var i=0;i<10;i++){
+	bst_add(root, parseInt(Math.random()*20));
+}
